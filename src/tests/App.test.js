be54cafeef709cecd1f02ac20ac3a3d37b27f382
Austin Howard <ahoward2@unca.edu>
2020-10-29 10:18:40 -0400

    1.1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,3 +1,31 @@
+import fs from 'fs';
+import util from 'util';
+
+
+const readFile = util.promisify(fs.readFile);
+const getJSONFile = async () => {
+  const JSONFile = await readFile('package.json');
+
+  if (!JSONFile){
+    throw new Error(`Could not find package.json`)
+  }
+  return JSONFile;
+}
+
+
 test("Tests working", () => {
   expect(1 + 1).toBe(2);
 });
+
+describe('dependencies', () => {
+
+  test("JSON data comes back", async () => {
+    const JSONFile = await getJSONFile();
+    const JSONData = JSON.parse(JSONFile);
+    const dependencies = JSONData.dependencies;
+
+    const materialUI = dependencies["@material-ui/core"];
+
+    expect(materialUI === undefined).toBe(false);
+  })
+})
